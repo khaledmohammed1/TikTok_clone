3312df58b6d7a9added8291f6a7c58993c2a3661
khaled <kha103led@yahoo.com>
2022-07-02 03:41:03 +0200

    continue development

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/controllers/auth_controller.dart';
 
+import 'views/screens/add_video_screen/add_video.dart';
+
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
@@ -17,7 +19,7 @@ var authController = AuthController.instance;
 List pages = const [
   Text('Video Screen'),
   Text('Search Screen'),
-  Text('Add Video Screen'),
+  AddVideoScreen(),
   Text('Messages Screen'),
   Text('Profile Screen '),
 
